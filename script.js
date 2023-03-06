9a3a93dbfdf126730305033ccdbfaf35fbfdc9f6
Manika <manika.aditya@gmail.com>
2023-03-06 19:02:54 +0530

    fully functional weather app

--- script.js
+++ script.js
@@ -18,5 +18,25 @@ const getWeather = (city) => {
 
 const showWeatherData = (weatherData) => {
 	console.log(weatherData)
-	document.getElementById('temp').innerText = weatherData.main.temp 
+	document.getElementById('temp').innerText = weatherData.main.temp
+	document.getElementById('min-temp').innerText = weatherData.main.temp_min
+	document.getElementById('max-temp').innerText = weatherData.main.temp_max
+	document.getElementById('pressure').innerText = weatherData.main.pressure
+	document.getElementById('humidity').innerText = weatherData.main.humidity
+	document.getElementById('weather-type').innerText = weatherData.weather[0].main
 }
+
+// const weather = weatherData.weather[0].main;
+// function setBackground(weather) {
+// 	if (weather == "Clouds") {
+// 	  body.background.setBackground = url("Clear.avif");
+// 	} else if (weather == "Haze") {
+// 	  background.src = "";
+// 	} else if (weather == "Cloud") {
+// 	  background.src = "";
+// 	} else if (weather == "Smoke") {
+// 	  background.src = "";
+// 	} else if (weather == "Rain") {
+// 	background.src = "";
+//   }
+//   }
